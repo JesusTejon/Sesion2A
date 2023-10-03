9a6132cb328174b5fefc8b8aaf8790f3a586251c
usuario <usuario@pc21-101>
2023-10-03 16:54:10 +0200

    prueba unitaria incluida

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -2,7 +2,21 @@ package pkg;
 
 public class Calculadora {
 	
-
+	public static int suma (int a, int b) {
+		return (a+b);
+	}
 	
-
+	public static int resta (int a, int b) {
+		return (a-b);
+	}
+	
+	public static int multipica (int a, int b) {
+		return (a*b);
+	}
+	
+	public static int divide (int a, int b) {
+		
+		if(b!=0) return (a/b);
+		else return -1;
+	}
 }
